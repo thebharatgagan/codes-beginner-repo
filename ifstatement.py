b1687683c6cfc24b11cfb33e9b07fb769cb83b83
Bharat Gagan <bharatgagan07@gmail.com>
2026-01-01 20:15:11 +0530

    conditionalstatment.py
    using conditional statement ( if, elif, else) in this program

---
+++ ifstatement.py
@@ -0,0 +1,30 @@
+# Conditional Statement is used for decision making in programming. 
+# We are Peforming with the help of if, elif and else statements.
+
+# Example 1: Simple if statement using user input
+
+age = int(input("Enter the age: "))
+if(age > 19):
+    print("You are adult.")
+    
+# Example 2: if-else statement
+
+marks = int(input("enter the marks: "))
+if(marks > 33):
+    print("you are passed in exam.")
+else:
+    print("you are failed in exam.")
+    
+# Example 3: if-elif-else statement
+
+marks = int(input("enter the marks: "))
+if(marks > 33):
+    print("you are passed in exam.")
+elif(marks == 33):
+    print("you are promoted in exam. ")
+else:
+    print("you are failed in exam.")
+    
+    
+# Thanks & End the Program.
+
