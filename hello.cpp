ca199c1015f75e591d20c08e06f293e67ce8bd64
Bharat Gagan <bharatgagan07@gmail.com>
2026-01-01 19:14:35 +0530

    Introductio.cpp

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hello World " << endl;
+    cout << "I am Bharat Gagan Currently Pursuing B.Sc in Computer Science from Delhi University." << endl;
+    return 0;
+}
